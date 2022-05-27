b8bb832b267a40cef6e314c9d9f6b68667cf5887
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2022-05-26 17:37:36 -0700

    react18

--- src/layouts/main.jsx
+++ src/layouts/main.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import ReactDOM from 'react-dom';
 import '../static/css/index.css';
 import App from './App.jsx';
 import { GlobalContextProvider } from '../../src/contexts/global/GlobalContext.jsx';
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
   <React.StrictMode>
     <GlobalContextProvider>
       <App />
     </GlobalContextProvider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );

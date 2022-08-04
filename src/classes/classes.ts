04d174a0d6269a4aad74384ced7cdf9fe13eeac1
Steve Yee <steveplaysaxe@hotmail.com>
2022-08-03 22:31:12 -0700

    add xstate

--- src/classes/classes.ts
+++ src/classes/classes.ts
@@ -4,11 +4,13 @@
 class Living {
   constructor(options: any) {
     const defaults = {
-      _isAlive: true,
-      name: 'noname',
-      age: 'default b age',
-      health: 'default c health',
+      _isAlive: true as boolean,
+      name: 'noname' as string,
+      age: 0 as number,
+      health: 'default c health' as string,
+      lastWords: 'hello world' as string,
     };
+    //setup defaults
     const populated = Object.assign(defaults, options);
     for (const key in populated) {
       if (populated.hasOwnProperty(key)) {
@@ -32,12 +34,12 @@ class Living {
   }
 }
 
-class Sentient extends Living {
+export class Sentient extends Living {
   constructor(options: any, extendedOptions: any) {
     const defaults = {
       int: 1,
-      atkPrc: 1,
-      defPrc: 1,
+      damage: 1,
+      absorbDamage: 1,
     };
     const populated = Object.assign(defaults, extendedOptions);
     super(options);
@@ -50,11 +52,15 @@ class Sentient extends Living {
 
   speak() {
     super.speak();
-    console.log(`Sentient speaks.`);
+    this.lastWords = 'I spoke!';
+    console.log(`${this.name} speaks.`);
+    console.log(`${this.lastWords}`);
   }
   defend(attacker: any) {
-    let incomingPierce = attacker.atkPrc;
-    let damage = incomingPierce - this.defPrc;
+    let incomingPierce = attacker.damage;
+    console.log('incomingPierce', incomingPierce);
+    console.log('', this.name, 'defends');
+    let damage = incomingPierce - this.absorbDamage;
 
     this.health -= damage <= 0 ? 0 : damage;
     if (this.health <= 0) {
@@ -68,9 +74,9 @@ class Sentient extends Living {
 
 export const cat: any = new Sentient(
   { name: 'cat', age: 2, health: 100 },
-  { int: 5, atkPrc: 14, defPrc: 2 }
+  { int: 5, damage: 14, absorbDamage: 32 }
 );
 export const dog = new Sentient(
   { name: 'dog', age: 21, health: 100 },
-  { int: 5, atkPrc: 8, defPrc: 5 }
+  { int: 5, damage: 38, absorbDamage: 5 }
 );

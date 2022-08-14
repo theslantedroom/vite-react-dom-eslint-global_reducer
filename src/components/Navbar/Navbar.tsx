53ad3f498d43bb4b8d9255ff36a13cf70b677cc1
Steve Yee <steveplaysaxe@hotmail.com>
2022-08-14 10:27:14 -0700

    dry nav and routes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
 import React, { useCallback, useState, useMemo } from 'react';
 
 import { Stack, Box, Divider, Chip, Link } from '@mui/material';
-import FaceIcon from '@mui/icons-material/Face';
+
+import { appRoutes } from './../../layouts/App';
 
 function Navbar() {
-  const handleClick = () => {};
   return (
     <Box
       sx={{
@@ -17,21 +17,13 @@ function Navbar() {
         justifyContent: 'space-between',
       }}
     >
-      <Link href="/" underline={'none'} className="text">
-        <Chip label="Home" onClick={handleClick} />
-      </Link>{' '}
-      <Link href="/SwipeablePage" underline={'none'} className="text">
-        <Chip label="SwipeablePage" onClick={handleClick} />
-      </Link>
-      <Link href="/EndlessScroll" underline={'none'} className="text">
-        <Chip label="EndlessScroll" onClick={handleClick} />
-      </Link>
-      <Link href="/FightPage" underline={'none'} className="text">
-        <Chip label="FightPage" onClick={handleClick} />
-      </Link>
-      <Link href="/StreetLightXstate" underline={'none'} className="text">
-        <Chip label="StreetLightXstate" onClick={handleClick} />
-      </Link>
+      {appRoutes.map((route, index) => {
+        return (
+          <Link href={route.path} underline={'none'}>
+            <Chip label={route.navText} />
+          </Link>
+        );
+      })}
     </Box>
   );
 }
--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,22 +1,32 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Pages
 import { Home } from '../pages/Home';
 import { StreetLightXstate } from '../pages/StreetLightXstate';
 import { FightPage } from '../pages/FightPage';
+import { BoxingLog } from '../pages/BoxingLog';
 import { EndlessScroll } from '../pages/EndlessScroll';
 import { SwipeablePage } from '../pages/SwipeablePage';
+
+export const appRoutes = [
+  { path: '/', element: <Home />, navText: 'Home' },
+  { path: '/EndlessScroll', element: <EndlessScroll />, navText: 'EndlessScroll' },
+  { path: '/SwipeablePage', element: <SwipeablePage />, navText: 'SwipeablePage' },
+  { path: '/StreetLightXstate', element: <StreetLightXstate />, navText: 'StreetLightXstate' },
+  { path: '/FightPage', element: <FightPage />, navText: 'FightPage' },
+  { path: '/BoxingLog', element: <BoxingLog />, navText: 'BoxingLog' },
+];
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/EndlessScroll" element={<EndlessScroll />} />
-        <Route path="/SwipeablePage" element={<SwipeablePage />} />
-        <Route path="/StreetLightXstate" element={<StreetLightXstate />} />
-        <Route path="/FightPage" element={<FightPage />} />
+        {appRoutes.map((route, index) => {
+          return <Route key={index} path={route.path} element={route.element} />;
+        })}
       </Routes>
     </BrowserRouter>
   );

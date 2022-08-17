bc9dc7d07f5b723ef4fca9928703874fcb182d65
Steve Yee <steveplaysaxe@hotmail.com>
2022-08-16 22:27:10 -0700

    add elapsed time

--- src/components/TimeDisplayFastTime/TimeDisplayFastTime.tsx
+++ src/components/TimeDisplayFastTime/TimeDisplayFastTime.tsx
@@ -30,14 +30,18 @@ export const TimeDisplayFastTime: React.FC<Props> = ({ message = 'Time' }) => {
   const realTimeOnRender = useRef(new Date());
   const [speedIndex, setSpeedIndex] = useState(1000);
   const [time, setTime] = useState(realTimeOnRender.current);
+  const [elapsedTime, setElapsedTime] = useState({ d: 0, h: 0, m: 0, s: 0 });
 
   useEffect(() => {
     console.log('Start Timer');
     const timer = setInterval(() => {
       const startDate = realTimeOnRender.current;
       const endDate = new Date();
-      var elapsedTime = endDate.getTime() - startDate.getTime();
+      const elapsedTime = endDate.getTime() - startDate.getTime();
+      const speedUpElapsedTime = elapsedTime * speedIndex;
 
+      const timeElpased = convertMiliseconds(speedUpElapsedTime);
+      setElapsedTime(timeElpased);
       const newTime = new Date(realTimeOnRender.current.getTime() + elapsedTime * speedIndex);
 
       setTime(newTime);
@@ -49,8 +53,19 @@ export const TimeDisplayFastTime: React.FC<Props> = ({ message = 'Time' }) => {
   }, [speedIndex, realTimeOnRender]);
 
   const timeDisplay = useMemo(() => {
-    console.log('time', time);
-    return <Typography variant="h3">{time.toLocaleString('en-US', dateOptions)}</Typography>;
+    return (
+      <>
+        <Typography variant="body2">{`speed: ${speedIndex}x `}</Typography>
+        <Typography variant="h3">{time.toLocaleString('en-US', dateOptions)}</Typography>
+        <Typography variant="caption">Time Past</Typography>
+        <Stack spacing={1} direction="row">
+          <Typography variant="caption">{`days: ${elapsedTime.d}`}</Typography>{' '}
+          <Typography variant="caption">{`hours: ${elapsedTime.h}`}</Typography>{' '}
+          <Typography variant="caption">{`minutes: ${elapsedTime.m}`}</Typography>{' '}
+          <Typography variant="caption">{`seconds: ${elapsedTime.s}`}</Typography>
+        </Stack>
+      </>
+    );
   }, [time, realTimeOnRender]);
 
   return (
@@ -59,13 +74,26 @@ export const TimeDisplayFastTime: React.FC<Props> = ({ message = 'Time' }) => {
 
       <Stack direction={'column'} justifyContent="space-between">
         <Typography variant="body2">{'started at:'}</Typography>
-
         <Typography variant="h6">
           {realTimeOnRender.current.toLocaleString('en-US', dateOptions)}
         </Typography>
-        <Typography variant="body2">{'now:'}</Typography>
         {timeDisplay}
       </Stack>
     </Box>
   );
 };
+
+function convertMiliseconds(miliseconds: number, format?: string) {
+  var days, hours, minutes, seconds, total_hours, total_minutes, total_seconds;
+
+  total_seconds = Math.floor(miliseconds / 1000);
+  total_minutes = Math.floor(total_seconds / 60);
+  total_hours = Math.floor(total_minutes / 60);
+  days = Math.floor(total_hours / 24);
+
+  seconds = total_seconds % 60;
+  minutes = total_minutes % 60;
+  hours = total_hours % 24;
+
+  return { d: days, h: hours, m: minutes, s: seconds };
+}

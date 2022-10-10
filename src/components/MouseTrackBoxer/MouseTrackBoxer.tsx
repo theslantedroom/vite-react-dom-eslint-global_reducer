a3fb34862b51f18a6a305d0bd911acd9923a5c8d
Steve Yee <steveplaysaxe@hotmail.com>
2022-10-10 12:24:00 -0700

    add gamepad boxing

--- src/components/MouseTrackBoxer/MouseTrackBoxer.tsx
+++ src/components/MouseTrackBoxer/MouseTrackBoxer.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState, useCallback, useEffect } from 'react';
 import useMouse from '@react-hook/mouse-position';
-import { styles } from '@dash-ui/styles';
 
-import { Typography, Box } from '@mui/material';
+import { useGamepads } from 'react-gamepads';
+import { GamepadsProvider } from 'react-gamepads';
+import { Typography, Box, Stack } from '@mui/material';
 const boardSize = 500;
 
 export interface Props {
@@ -10,164 +11,91 @@ export interface Props {
   required: string;
 }
 export const MouseTrackBoxer: React.FC<Props> = ({ optional = true, required }) => {
-  return (
-    <>
-      <Draw />
-    </>
-  );
-};
+  const [gamepads, setGamepads] = useState<any>({});
+  const [leftYScale, setLeftYScale] = useState(Math.round(window.innerHeight / 2));
+  const [leftXScale, setLeftXScale] = useState(Math.round(window.innerWidth / 2));
+  const [rightYScale, setRightYScale] = useState(Math.round(window.innerHeight / 2));
+  const [rightXScale, setRightXScale] = useState(Math.round(window.innerWidth / 2));
+  useGamepads((gamepads) => setGamepads(gamepads));
+  useEffect(() => {
+    const defaultGamepad = Object.keys(gamepads).length > 0 ? gamepads[0] : {};
+    if ('buttons' in defaultGamepad) {
+      // console.log('button press');
+    }
+    if ('axes' in defaultGamepad) {
+      setLeftYScale(defaultGamepad.axes[1]);
+      setLeftXScale(defaultGamepad.axes[0]);
+      setRightYScale(defaultGamepad.axes[3]);
+      setRightXScale(defaultGamepad.axes[2]);
+    }
+  }, [gamepads]);
 
-function Draw() {
-  const target = useRef(null);
-  const [isControlling, setIsControlling] = useState(true);
-  const [useRightHand, setUseRightHand] = useState(true);
+  const headSize = boardSize / 5;
+  const fistSize = headSize / 2;
 
-  const player1 = {
-    head: <div></div>,
-    left: <div></div>,
-    right: <div></div>,
+  const leftHandStartX = boardSize / 2 - fistSize / 2 - fistSize / 2;
+  const leftHandStartY = boardSize / 4;
+
+  const rightHandStartX = boardSize / 2 - fistSize / 2 + fistSize / 2;
+  const rightHandStartY = boardSize / 4;
+  const player2 = {
+    head: (
+      <div
+        style={{
+          position: 'absolute' as any,
+          top: `${boardSize / 10}px`,
+          left: `${boardSize / 2 - headSize / 2}px`,
+          width: headSize,
+          height: headSize,
+          backgroundColor: '#eebb99',
+          borderRadius: 1000,
+        }}
+      />
+    ),
+    leftHand: (
+      <div
+        style={{
+          position: 'absolute' as any,
+          top: `${leftHandStartY + leftYScale * leftHandStartY}px`,
+          left: `${leftHandStartX + leftXScale * leftHandStartX}px`,
+          width: fistSize,
+          height: fistSize,
+          backgroundColor: 'blue',
+          borderRadius: 1000,
+        }}
+      />
+    ),
+    rightHand: (
+      <div
+        style={{
+          position: 'absolute' as any,
+          top: `${rightHandStartY + rightYScale * rightHandStartY}px`,
+          left: `${rightHandStartX + rightXScale * rightHandStartX}px`,
+          width: fistSize,
+          height: fistSize,
+          backgroundColor: 'blue',
+          borderRadius: 1000,
+        }}
+      />
+    ),
   } as any;
 
-  const mouse = useMouse(target, {
-    fps: Infinity,
-    enterDelay: 100,
-    leaveDelay: 100,
-  });
-
-  useEffect(() => {
-    if (!mouse.elementHeight || !mouse.elementWidth || !mouse.x || !mouse.y) {
-      return;
-    }
-    if (mouse.y < mouse.elementHeight / 2) {
-      return;
-    }
-
-    setUseRightHand(mouse.x > mouse.elementWidth / 2);
-  }, [mouse]);
-
-  const headStyle = useCallback((): React.CSSProperties => {
-    const headSize = boardSize / 5;
-    const headCss = {
-      position: 'absolute' as any,
-      bottom: `${boardSize / 10}px`,
-      left: `${boardSize / 2 - headSize / 2}px`,
-      width: headSize,
-      height: headSize,
-      backgroundColor: '#eebb99',
-      borderRadius: 1000,
-    };
-
-    if (!mouse.elementHeight || !mouse.elementWidth || !mouse.x || !mouse.y || !isControlling) {
-      return headCss;
-    }
-
-    const xMod = mouse.elementWidth / 2 - headSize / 2 + mouse.x / 10;
-    const yMod = mouse.elementHeight - 200 + mouse.y / 10;
-
-    const headStyleCss = {
-      width: headSize,
-      height: headSize,
-      left: Math.min(xMod, mouse.elementHeight),
-      top: Math.min(yMod, mouse.elementHeight),
-    } as React.CSSProperties;
-
-    return { ...headCss, ...headStyleCss };
-  }, [mouse, isControlling]);
-
-  const fistStyle = useCallback(
-    (options: any): React.CSSProperties => {
-      const { isLead, isActive } = options;
-      const fistSize = 50;
-      const guardGap = fistSize * 0.5;
-      const fistCss = {
-        position: 'absolute' as any,
-        bottom: `${boardSize / 4}px`,
-        left: `${
-          boardSize / 2 - fistSize / 2 + (isLead ? (boardSize / 15) * -1 : boardSize / 15)
-        }px`,
-        width: fistSize,
-        height: fistSize,
-        backgroundColor: 'red',
-        borderRadius: 1000,
-      };
-
-      if (!mouse.elementHeight || !mouse.elementWidth || !mouse.x || !mouse.y || !isControlling) {
-        return fistCss;
-      }
-
-      const xMod = mouse.x + (isLead ? guardGap : -guardGap);
-      const yMod = mouse.y;
-      const fistStyleCss = {
-        width: fistSize,
-        height: fistSize,
-        left: Math.min(xMod, mouse.elementHeight + fistSize / 2),
-        top: Math.min(yMod, mouse.elementHeight + fistSize / 2),
-      } as React.CSSProperties;
-
-      const xModB =
-        mouse.elementWidth / 2 - fistSize / 2 + mouse.x / 10 + (isLead ? guardGap : -guardGap) + 1;
-      const yModB = mouse.elementHeight - 200 + mouse.y / 10 - mouse.y / 10;
-      const isActiveCss = isActive
-        ? {
-            border: '2px solid yellow',
-          }
-        : ({
-            width: fistSize,
-            height: fistSize,
-            left: Math.min(xModB, mouse.elementHeight),
-            top: Math.min(yModB, mouse.elementHeight),
-          } as React.CSSProperties);
-
-      return { ...fistCss, ...fistStyleCss, ...isActiveCss };
-    },
-    [mouse, isControlling]
-  );
-
-  player1.head = (
-    <div
-      onClick={() => setIsControlling(true)}
-      style={{
-        ...headStyle(),
-      }}
-    />
-  );
-
-  if (mouse.x && mouse.elementWidth && mouse.y && mouse.elementHeight) {
-    player1.left = (
-      <div
-        style={{
-          ...fistStyle({ isLead: true, isActive: useRightHand }),
-        }}
-      />
-    );
-    player1.right = (
-      <div
-        style={{
-          ...fistStyle({ isLead: false, isActive: !useRightHand }),
-        }}
-      />
-    );
-  }
-
-  // if (isControlling) defCss.cursor = 'none';
   return (
-    <div>
-      <Box sx={defCss} ref={target}>
-        {player1.head}
-        {player1.left}
-        {player1.right}
-        {/* {drawing} */}
+    <Stack>
+      <Box sx={defCss}>
+        <GamepadsProvider>
+          {player2.head}
+          {player2.leftHand}
+          {player2.rightHand}
+        </GamepadsProvider>
       </Box>
-      <div>{useRightHand ? 'left' : 'right'}</div>
-      <div>
-        x:{mouse.x} / {mouse.elementWidth}
-      </div>
-      <div>
-        y:{mouse.y} / {mouse.elementHeight}
-      </div>
-    </div>
+      <div>leftYScale:{leftYScale}</div>
+      <div>leftXScale:{leftXScale}</div>
+      <div>rightYScale:{rightYScale}</div>
+      <div>rightXScale:{rightXScale}</div>
+    </Stack>
   );
-}
+};
 
 const defCss = {
   overflow: 'hidden',
---
+++ src/components/MouseTrackBoxer/MouseTrackBoxerMouseControl.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { Meta, Story } from '@storybook/react';
+import { MouseTrackBoxerMouseControl, Props } from './MouseTrackBoxerMouseControl';
+export default {
+  title: 'MouseTrackBoxer/MouseTrackBoxerMouseControl',
+  component: MouseTrackBoxerMouseControl,
+} as Meta;
+
+const Template: Story<Props> = (args) => <MouseTrackBoxerMouseControl {...args} />;
+
+export const Default = Template.bind({});
+Default.args = { optional: true, required: 'required' };
